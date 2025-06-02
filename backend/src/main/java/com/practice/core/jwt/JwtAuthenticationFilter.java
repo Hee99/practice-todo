84bdd023b7372e324267bfec25c217cd327f4395
Hee99 <119tpgml@gmail.com>
2025-06-02 17:48:26 +0900

    소스정리

--- backend/src/main/java/com/practice/core/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/practice/core/jwt/JwtAuthenticationFilter.java
@@ -20,7 +20,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         // 1. Request Header에서 jwt 토큰 추출
-        String token = resolveToken((HttpServletRequest) request);
+        String token = resolveToken(request);
 
         // 2. 토큰 유효성 검사
         if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
